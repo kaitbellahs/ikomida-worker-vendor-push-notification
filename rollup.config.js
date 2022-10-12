d3dc141eebde8a6f1bb60e64c88b3da60e8c326a
khalid ait bellahs <khalid@tialtonivel.com.br>
2022-10-11 22:11:21 -0300

    Update workflow
    Update deployment
    Update libs
    Update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,10 @@ import tsPlugin from '@rollup/plugin-typescript';
 import resolve from "@rollup/plugin-node-resolve";
 import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
-import pkg from "./package.json";
-import tsconfig from './tsconfig.json';
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const pkg = require('./package.json')
+const tsconfig = require('./tsconfig.json')
 
 export default [
     {
